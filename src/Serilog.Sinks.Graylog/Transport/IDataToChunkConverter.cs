5f6ddfd470588977dfa97313e3e989584f9a695f
Dennis Larsen <dennis.larsen@gmail.com>
2017-09-13 23:24:51 +0200

    Extracted interface

---
+++ src/Serilog.Sinks.Graylog/Transport/IDataToChunkConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Serilog.Sinks.Graylog.Transport
+{
+    /// <summary>
+    /// Converts Data to TCP and UDP chunks
+    /// </summary>
+    public interface IDataToChunkConverter
+    {
+        /// <summary>
+        /// Converts to chunks.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <returns>array of chunks to save</returns>
+        /// <exception cref="System.ArgumentException">message was too long</exception>
+        /// <exception cref="ArgumentException">message was too long</exception>
+        IList<byte[]> ConvertToChunks(byte[] message);
+    }
+}
