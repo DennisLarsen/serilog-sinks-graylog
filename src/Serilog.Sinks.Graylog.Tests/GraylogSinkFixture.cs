486e1c9fe5533f4f36954e3ebafa5ab859f940c9
Dennis Larsen <dennis.larsen@gmail.com>
2017-09-13 23:21:31 +0200

    Added igoned test for Tcp and Http. Only reason is to show them

--- src/Serilog.Sinks.Graylog.Tests/GraylogSinkFixture.cs
+++ src/Serilog.Sinks.Graylog.Tests/GraylogSinkFixture.cs
@@ -14,8 +14,7 @@ namespace Serilog.Sinks.Graylog.Tests
     {
 
         [Fact(Skip = "This test not work anymore because IMessageBuilder gets from internal dictionary")]
-        
-        public void WhenEmit_ThenSendData()
+        public void WhenEmit_ThenSendData_for_Transport_udp()
         {
             var gelfConverter = new Mock<IGelfConverter>();
             var transport = new Mock<ITransport>();
@@ -24,6 +23,68 @@ namespace Serilog.Sinks.Graylog.Tests
             {
                 GelfConverter = gelfConverter.Object,
                 TransportType = TransportType.Udp,
+                HostnameOrAdress = "localhost"
+            };
+
+            GraylogSink target = new GraylogSink(options);
+
+            var logevent = new LogEvent(DateTimeOffset.Now, LogEventLevel.Fatal, null,
+                new MessageTemplate("O_o", new List<MessageTemplateToken>()), new List<LogEventProperty>());
+
+            var jobject = new JObject();
+            transport.Setup(c => c.Send(jobject.ToString(Newtonsoft.Json.Formatting.None))).Returns(Task.CompletedTask);
+
+
+            gelfConverter.Setup(c => c.GetGelfJson(logevent)).Returns(jobject);
+
+            target.Emit(logevent);
+
+            gelfConverter.VerifyAll();
+
+            transport.Verify(c => c.Send(It.IsAny<string>()));
+        }
+
+        [Fact(Skip = "This test not work anymore because IMessageBuilder gets from internal dictionary")]
+        public void WhenEmit_ThenSendData_Transport_tcp()
+        {
+            var gelfConverter = new Mock<IGelfConverter>();
+            var transport = new Mock<ITransport>();
+
+            var options = new GraylogSinkOptions
+            {
+                GelfConverter = gelfConverter.Object,
+                TransportType = TransportType.Tcp,
+                HostnameOrAdress = "localhost"
+            };
+
+            GraylogSink target = new GraylogSink(options);
+
+            var logevent = new LogEvent(DateTimeOffset.Now, LogEventLevel.Fatal, null,
+                new MessageTemplate("O_o", new List<MessageTemplateToken>()), new List<LogEventProperty>());
+
+            var jobject = new JObject();
+            transport.Setup(c => c.Send(jobject.ToString(Newtonsoft.Json.Formatting.None))).Returns(Task.CompletedTask);
+
+
+            gelfConverter.Setup(c => c.GetGelfJson(logevent)).Returns(jobject);
+
+            target.Emit(logevent);
+
+            gelfConverter.VerifyAll();
+
+            transport.Verify(c => c.Send(It.IsAny<string>()));
+        }
+
+        [Fact(Skip = "This test not work anymore because IMessageBuilder gets from internal dictionary")]
+        public void WhenEmit_ThenSendData_Transport_http()
+        {
+            var gelfConverter = new Mock<IGelfConverter>();
+            var transport = new Mock<ITransport>();
+
+            var options = new GraylogSinkOptions
+            {
+                GelfConverter = gelfConverter.Object,
+                TransportType = TransportType.Http,
                 HostnameOrAdress = "localhost"
             };
 
